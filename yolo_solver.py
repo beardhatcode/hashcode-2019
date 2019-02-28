1da8d0123dcaef23f417420695532a98d0aaaa33
Robbert Gurdeep Singh <mail.garonn@gmail.com>
2019-02-28 22:08:44 +0100

    Update yolo_solver.py

--- yolo_solver.py
+++ yolo_solver.py
@@ -1,27 +1,49 @@
 from utils import score
+#from start import Image
 import random
+from collections import namedtuple
 
-def solver(images, horizontal, vertical, maxtag):
-    groups=maxtag*[None]
+
+
+ImageH = namedtuple('ImageH', ['num', 'tags'])
+
+
+def makeH(vertical: set()):
+    l = list(vertical)
+    random.shuffle(l)
+    return [ImageH((l[k].num,l[k+1].num), l[k].tags | l[k+1].tags) for k in range(0,len(l),2)]
+
+def conv( i ):
+    if isinstance(i, tuple):
+        return list(i)
+    else:
+        return [i]
+
+
+def solver(images, horizontal: set(), vertical: set(), maxtag) -> [[int]]:
+
+
+    groups = maxtag*[None]
     for i in range(maxtag):
         groups[i] = set()
-    
+
+    images = horizontal | set(makeH(vertical))
+
     for i in images:
-        if i.orientation:
-            for t in i.tags:
-                groups[t].add(i)
-    
-    #print(groups)
+        for t in i.tags:
+            groups[t].add(i)
+
+    # print(groups)
 
     used = set()
-    slides  = []
+    slides = []
     for i in images:
-        if i not in used and i.orientation:
+        if i not in used:
             used.add(i)
             curTags = i.tags
             buddys = [groups[t] - used for t in curTags]
             used.remove(i)
-            
+
             buddysS = {}
             for bl in buddys:
                 for b in bl:
@@ -29,42 +51,11 @@ def solver(images, horizontal, vertical, maxtag):
                         buddysS[b] = 1
                     else:
                         buddysS[b] += 1
-            similarsH = [i for i,c in buddysS.items() if c > 1 and i.orientation == "H"]
-            similarsV = [i for i,c in buddysS.items() if c > 1 and i.orientation == "V"]
-            if i.orientation == "H":
-                if len(similarsV) >= 2:
-                    slides.append([i.num])
-                    slides.append([similarsV[0].num,similarsV[1].num])
-                    used.add(similarsV[0])
-                    used.add(similarsV[1])
-                    used.add(i)
-                elif similarsH:
-                    slides.append([i.num])
-                    slides.append([similarsH[0].num])
-                    used.add(similarsH[0])
-                    used.add(i)
-            else: # V
-                if len(similarsV) >= 2 or similarsH:
-                    for _ in range(30):
-                        j = random.choice(images)
-                        if j.orientation == 'V' and j not in used:
-                            similarsV2 = [i for i in similarsV if i.num != j.num]
-                            similarsH2 = [i for i in similarsH if i.num != j.num]
-                            if len(similarsV2) >= 2:
-                                slides.append([i.num, j.num])
-                                slides.append([similarsV2[0].num,similarsV2[1].num])
-                                used.add(similarsV2[0])
-                                used.add(similarsV2[1])
-                                used.add(i)
-                                used.add(j)
-                            elif similarsH2:
-                                slides.append([i.num])
-                                slides.append([similarsH2[0].num])
-                                used.add(similarsH2[0])
-                                used.add(i)
-                            break
-
-                
+            similars = [i for i, c in buddysS.items() if c > 1]
+            if similars:
+                slides.append(conv(i.num))
+                slides.append(conv(similars[0].num))
+                used.add(similars[0])
+                used.add(i)
 
     return slides
-            

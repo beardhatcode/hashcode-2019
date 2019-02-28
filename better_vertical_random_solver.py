81e9d0a7ca39d3bed8a099c9ca53739c80dfe99e
Felix Van der Jeugt <felix.vanderjeugt@gmail.com>
2019-02-28 22:08:45 +0100

    use sliding window in vertical better

--- better_vertical_random_solver.py
+++ better_vertical_random_solver.py
@@ -1,16 +1,29 @@
 import random
+from collections import deque
+
+SIZE = 50
 
 def solver(tags, horizontal, vertical, _num_tags):
     slides = list(map(lambda x: [x.num], list(horizontal)))
+
     vertical = [v.num for v in vertical]
     vertical.sort(key=lambda i: len(tags[i]))
+    window, vertical = vertical[:SIZE], deque(vertical[SIZE:])
 
-    while vertical:
-        a = vertical[-1]
-        j, b = max(enumerate(vertical), key=lambda t: len(tags[t[1]].tags - tags[a].tags))
-        del vertical[j]
-        del vertical[-1]
+    while window:
+        ia, a, ib, b = max(
+            ((ia, a, ib, b) for ia, a in enumerate(window) for ib, b in enumerate(window)),
+            key=lambda t4: len(tags[t4[1]].tags | tags[t4[3]].tags)
+        )
+        ia, ib = sorted([ia, ib])
+        del window[ib]
+        del window[ia]
         slides.append([a, b])
+        try:
+            window.append(vertical.popleft())
+            window.append(vertical.popleft())
+        except IndexError:
+            pass
 
     random.shuffle(slides)
     return slides

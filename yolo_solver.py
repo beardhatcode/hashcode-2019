6c8247833c370bc6686d5cab6836b7ff575251ef
beardhatcode <mail.garonn@gmail.com>
2019-02-28 21:21:57 +0100

    yolo

--- yolo_solver.py
+++ yolo_solver.py
@@ -7,7 +7,7 @@ def solver(images, horizontal, vertical, maxtag):
         groups[i] = set()
     
     for i in images:
-        if i.orientation == "H":
+        if i.orientation:
             for t in i.tags:
                 groups[t].add(i)
     
@@ -16,10 +16,11 @@ def solver(images, horizontal, vertical, maxtag):
     used = set()
     slides  = []
     for i in images:
-        if i not in used and i.orientation == "H":
+        if i not in used and i.orientation:
             used.add(i)
             curTags = i.tags
             buddys = [groups[t] - used for t in curTags]
+            used.remove(i)
             
             buddysS = {}
             for bl in buddys:
@@ -28,13 +29,42 @@ def solver(images, horizontal, vertical, maxtag):
                         buddysS[b] = 1
                     else:
                         buddysS[b] += 1
-            similars = [i for i,c in buddysS.items() if c > 0]
-            if len(similars) > 0:
-                slides.append([i.num])
-                slides.append([similars[0].num])
-                used.add(similars[0])
-            else:
-                used.remove(i)
+            similarsH = [i for i,c in buddysS.items() if c > 1 and i.orientation == "H"]
+            similarsV = [i for i,c in buddysS.items() if c > 1 and i.orientation == "V"]
+            if i.orientation == "H":
+                if len(similarsV) >= 2:
+                    slides.append([i.num])
+                    slides.append([similarsV[0].num,similarsV[1].num])
+                    used.add(similarsV[0])
+                    used.add(similarsV[1])
+                    used.add(i)
+                elif similarsH:
+                    slides.append([i.num])
+                    slides.append([similarsH[0].num])
+                    used.add(similarsH[0])
+                    used.add(i)
+            else: # V
+                if len(similarsV) >= 2 or similarsH:
+                    for _ in range(30):
+                        j = random.choice(images)
+                        if j.orientation == 'V' and j not in used:
+                            similarsV2 = [i for i in similarsV if i.num != j.num]
+                            similarsH2 = [i for i in similarsH if i.num != j.num]
+                            if len(similarsV2) >= 2:
+                                slides.append([i.num, j.num])
+                                slides.append([similarsV2[0].num,similarsV2[1].num])
+                                used.add(similarsV2[0])
+                                used.add(similarsV2[1])
+                                used.add(i)
+                                used.add(j)
+                            elif similarsH2:
+                                slides.append([i.num])
+                                slides.append([similarsH2[0].num])
+                                used.add(similarsH2[0])
+                                used.add(i)
+                            break
+
+                
 
     return slides
             

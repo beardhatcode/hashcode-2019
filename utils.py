2ce97e5b549ee29d6e10534f83574e43b976ec6d
Charlotte Van Petegem <charlotte.vanpetegem@ugent.be>
2019-02-28 20:34:06 +0100

    add eprint to utils

--- utils.py
+++ utils.py
@@ -1,7 +1,16 @@
+import sys
+
+
 def score(slides, images):
     s = 0
     for i in range(len(slides) - 1):
-        tags1 = images[slides[i][0]].tags if len(slides[i]) == 1 else images[slides[i][0]].tags | images[slides[i][1]].tags
-        tags2 = images[slides[i + 1][0]].tags if len(slides[i + 1]) == 1 else images[slides[i + 1][0]].tags | images[slides[i + 1][1]].tags
+        tags1 = images[slides[i][0]].tags if len(slides[i]) == 1 else images[slides[i][0]].tags | images[
+            slides[i][1]].tags
+        tags2 = images[slides[i + 1][0]].tags if len(slides[i + 1]) == 1 else images[slides[i + 1][0]].tags | images[
+            slides[i + 1][1]].tags
         s += min(len(tags1 & tags2), len(tags1 - tags2), len(tags2 - tags1))
     return s
+
+
+def eprint(*args, **kwargs):
+    print(*args, **kwargs, file=sys.stderr)
